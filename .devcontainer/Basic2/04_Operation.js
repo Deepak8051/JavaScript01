bb932a830f7fa08e6533f7b8c3e786089a552eb6
Deepak kumar Sharma <101473013+Deepak8051@users.noreply.github.com>
2023-09-04 12:45:40 +0000

    Operation

---
+++ .devcontainer/Basic2/04_Operation.js
@@ -0,0 +1,28 @@
+let value = 3
+let negValue = -value
+console.log(negValue);
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/3);
+console.log(2%2);
+
+let str1 =  "Deepak"
+let str2 =  " kumar"
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1" + 2 + 2);
+console.log(1+2+"2");
+
+console.log(+ true);// value is 1
+console.log(+ "");// value is 0
+
+let num1 , num2, num3
+num1 = num2 = num3  = 2+2
+let gameCounter  = 100
+gameCounter++;
+console.log(gameCounter);
