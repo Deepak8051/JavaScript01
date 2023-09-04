c11f613421522589024d0a8a0dadccf9a37eef95
Deepak kumar Sharma <101473013+Deepak8051@users.noreply.github.com>
2023-09-04 12:23:18 +0000

    conversion of dataType

---
+++ .devcontainer/Basic2/03_Conversion.js
@@ -0,0 +1,25 @@
+let score = "33"
+console.log(typeof score);
+console.log(typeof(score)); //by method check
+
+let valueInNumber = Number (score);
+console.log(typeof valueInNumber);
+
+
+//notes
+ //"33" =>33
+ //"33abc" =>nan(not a number)
+ //trure => 1
+ //false => 0
+
+ let isLoggedIn = 1
+ let booleanIsLoggedin = Boolean(isLoggedIn)
+ console.log(booleanIsLoggedin);
+
+ let someNumber = 33
+let stringNumber = String(someNumber)
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
+
+ 
+ 
