d7f31f983ed889d7558dcf2627646c9e3b0ea464
Deepak kumar Sharma <101473013+Deepak8051@users.noreply.github.com>
2023-09-08 18:20:12 +0000

    String Method

---
+++ .devcontainer/Basic2/String.js
@@ -0,0 +1,15 @@
+const name = "Deepak"
+const repoCount = 50
+//console.log(name + repoCount + "value");
+console.log(`Hello my name is ${name}and my repo count is ${repoCount}`);
+
+let Name = "Deepak"
+console.log(Name);
+console.log(Name.length);
+console.log(Name.toLowerCase());
+console.log(Name.toUpperCase());
+console.log(Name.charAt());
+console.log(Name.includes());
+console.log(Name.concat());
+console.log(Name.endsWith());
+console.log(Name.replace());
