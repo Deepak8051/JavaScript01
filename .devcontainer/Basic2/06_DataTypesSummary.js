3a37ff9fcedde465cc438a04a1f24baac4267d95
Deepak kumar Sharma <101473013+Deepak8051@users.noreply.github.com>
2023-09-08 17:22:43 +0000

    modify

---
+++ .devcontainer/Basic2/06_DataTypesSummary.js
@@ -0,0 +1,27 @@
+//Primitive DataType 
+// there are seven category
+//these are :- String , Number,Boolean,null,undifeined,symbol,bigInt
+//RefrenceType or Non promitive 
+//arrray ,objects , function
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outSideTemp = null
+let userEmail;
+//const id = s('123')
+console.log(id === anotherId)
+const BigInt = 1234567890n
+//array
+const heros = ["Deepak","kumar"]
+//object
+let myObj= {
+     name: "Deepak",
+     age :  24,
+
+}
+ const Myfunction = function(){
+    console.log("HelloWorld");
+
+ }
+ console.log(typeof BigInt);
+
